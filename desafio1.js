9559e423480271d03c3e666a88397084c3541f68
Nicolas Martin Reynoso <nico97lp@hotmail.com>
2023-04-11 09:59:46 -0300

    desafio1

--- desafio1.js
+++ desafio1.js
@@ -1,14 +1,3 @@
-
-
-// Debe crearse desde su constructor con el elemento products, el cual será un arreglo vacío.
-// Cada producto que gestione debe contar con las propiedades:
-// title (nombre del producto)
-// description (descripción del producto)
-// price (precio)
-// thumbnail (ruta de imagen)
-// code (código identificador)
-// stock (número de piezas disponibles)
-
 class ProductManager {
     constructor() {
         this.products = [];
